6db772f1340cfa491216e8cd9acca1dd4b1063e8
Edivaldo Ramos <edivaldoramossj@gmail.com>
2020-05-01 22:11:11 -0300

    Primeiro commit

---
+++ src/main/java/com/github/edivaldoramos/ExemploSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.github.edivaldoramos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExemploSpringSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExemploSpringSecurityApplication.class, args);
+    }
+
+}
